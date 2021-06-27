fd58dffd8c944416ed5c053acfcfc888fb814ed8
dazhaolear <68630509+dazhaolear@users.noreply.github.com>
2021-06-27 10:58:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -40,3 +40,5 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
 #高级重启
 git clone https://github.com/dazhaolear/luci-app-autorebootnew.git package/luci-app-autorebootnew
+#网速测试
+git clone https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
